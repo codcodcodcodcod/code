aab68be69df31400e63a29af67c4d105f5afdd42
codcodcodcodcod <cordia.chiu@carbonlinking.com>
2023-07-24 14:58:46 +0800

    new update

--- math operatrion.js
+++ math operatrion.js
@@ -1,27 +1,45 @@
 
 
 // take the operator input
-const operator = prompt('Enter operator ( either +, -, * or / ): ');
+const operator = prompt('Enter operator ( either +, -, * , ^ , / or ^2');
 
 // take the operand input
 const number1 = parseFloat(prompt('Enter first number: '));
-const number2 = parseFloat(prompt('Enter second number: '));
+let number2;
+if (operator != "^2") {
+    number2 = parseFloat(prompt('Enter second number: '));
+}
+
 
 let result;
 
 // using if...else if... else
 if (operator == '+') {
     result = number1 + number2;
+    alert(result)
 }
 else if (operator == '-') {
     result = number1 - number2;
+    alert(result)
 }
 else if (operator == '*') {
     result = number1 * number2;
-}
-else {
-    result = number1 / number2;
+    alert(result)
 }
 
-// display the result
-console.log(`${number1} ${operator} ${number2} = ${result}`);
+else if (operator == '^'){
+    alert(Math.pow(number1,number2));
+ 
+}
+else if (operator == '/'){
+    result = number1 / number2;
+    alert(result)
+}
+else if (operator == '^2'){
+    alert(Math.pow(number1,2));
+}
+
+
+else {alert('error')}
+
+
